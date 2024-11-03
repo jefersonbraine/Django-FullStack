5cba553f421468f81c0a7e8c45e95066de4e409d
Jeferson <jefeleal363@gmail.com>
2024-11-03 15:48:14 -0300

    feat: db init, presents name and imgs

--- noivos/views.py
+++ noivos/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
+    if request.method == "GET":
+        return render(request, 'home.html')
+    elif request.method == "POST":
+        nome_presente = request.POST.get('nome_presente')
+        preco = request.POST.get('preco')
+        importancia = int(request.POST.get('importancia'))
+        foto = request.FILES.get('foto')
 
-    return render(request, 'home.html')
+        if importancia < 1 or importancia > 5:
+            return redirect('home')
+        return HttpResponse('teste')
